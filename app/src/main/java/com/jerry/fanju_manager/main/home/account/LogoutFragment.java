294b07d40548489c2515c7a4ce671b4157b0f0a5
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-06-30 16:37:09 +0200

    add AccountFragment

--- app/src/main/java/com/jerry/fanju_manager/main/home/account/LogoutFragment.java
+++ app/src/main/java/com/jerry/fanju_manager/main/home/account/LogoutFragment.java
@@ -1,4 +1,4 @@
-package com.jerry.fanju_manager.main.home;
+package com.jerry.fanju_manager.main.home.account;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -7,7 +7,6 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
